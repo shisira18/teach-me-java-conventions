a73d28ab056294c65bc9dd279114005405de0406
Shisira Unni <47895225+shisira18@users.noreply.github.com>
2022-03-21 20:21:06 +0530

    Clean Code

--- src/main/java/powerpackage/powerfinder.java
+++ src/main/java/powerpackage/powerfinder.java
@@ -1,7 +1,7 @@
 package powerpackage;
 
 public class powerfinder {
-    public static int OF(int number, int power) {
+    public static int powerCompute(int number, int power) {
         int result = 1;
         for (int i = 0; i<power; i++) {
             result *= number;
