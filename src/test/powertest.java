c2b1ac6d8cef835bbcedb81f0a63a246e8bb7577
Shisira Unni <47895225+shisira18@users.noreply.github.com>
2022-03-21 20:26:10 +0530

    Clean Code

--- src/test/powertest.java
+++ src/test/powertest.java
@@ -5,22 +5,22 @@ import org.junit.Test;
 
 public class powertest {
     @Test
-    public void one_raised_to_one_is_onew() {
-        assert powerfinder.OF(1,1) == 1;
+    public void oneRaisedToOneIsOne() {
+        assert powerfinder.powerCompute(1,1) == 1;
     }
 
     @Test
-    public void twoRaised_to_one_is_2() {
-        assert powerfinder.OF(2,1) == 2;
+    public void twoRaisedToTwoIsTwo() {
+        assert powerfinder.powerCompute(2,1) == 2;
     }
 
     @Test
-    public void two_power_2_is_4() {
-        assert powerfinder.OF(2, 2) == 4;
+    public void twoPowerTwoIsFour() {
+        assert powerfinder.powerCompute(2, 2) == 4;
     }
 
     @Test
-    public void power_of_2_and_3_Is_SIX() {
-        assert powerfinder.OF(3, 2) == 3*3;
+    public void powerOfTwoAndThreeIsNine() {
+        assert powerfinder.powerCompute(3, 2) == 3*3;
     }
 }
